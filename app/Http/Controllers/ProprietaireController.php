228784dba483b6dbe896064be13050052078b22a
MirajMalado <diallomalado8@gmail.com>
2022-04-06 16:03:12 +0000

    Create models

---
+++ app/Http/Controllers/ProprietaireController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\proprietaire;
+use Illuminate\Http\Request;
+
+class ProprietaireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\proprietaire  $proprietaire
+     * @return \Illuminate\Http\Response
+     */
+    public function show(proprietaire $proprietaire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\proprietaire  $proprietaire
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(proprietaire $proprietaire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\proprietaire  $proprietaire
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, proprietaire $proprietaire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\proprietaire  $proprietaire
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(proprietaire $proprietaire)
+    {
+        //
+    }
+}
